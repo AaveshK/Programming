30ee0e92baa8ee6d05e308a5f149764ac8b2ceb5
Aavesh K <Aavesh.bit@gmail.com>
2021-08-11 14:49:23 +0530

    Codechef Long August

--- Codechef/AUG21C-ARRFILL.java
+++ Codechef/AUG21C-ARRFILL.java
@@ -31,6 +31,14 @@ class Codechef
     static int min(int x, int y){return ((x < y) ? x : y );}
     static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
     static long gcd(long a, long b){if (b == 0)return a;return gcd(b, a % b);}
+    static class Pair
+    {
+        int x;
+        int y;
+        public Pair(int x, int y){this.x = x; this.y = y;}
+        public int getX(){return this.x;}
+        public int getY(){return this.y;}
+    }
     public static void main (String[] args) throws IOException
 	{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -41,24 +49,21 @@ class Codechef
             String[] strs = lines.trim().split("\\s+");
             long N = Long.parseLong(strs[0]);
             int M = Integer.parseInt(strs[1]);
-
-            Map<Long, Long> treemap = new TreeMap<Long, Long>(Collections.reverseOrder());
+            ArrayList<Pair> list = new ArrayList<>();
+            
             for(int j = 0; j<M; j++)
             {
                 lines = in.readLine();
                 strs = lines.trim().split("\\s+");
-                treemap.put(Long.parseLong(strs[0]),Long.parseLong(strs[1]));
+                list.add(new Pair((int)Long.parseLong(strs[0]),(int)Long.parseLong(strs[1])));
             }
-
-            Set set = treemap.entrySet();
-            Iterator i = set.iterator();
+            sortList(list);
 
             long lcm=1, rem=N, ans=0;
-            //for(int j =0; j< M && rem > 0; j++)
-            while(i.hasNext() && rem > 0)
+            for(int j = (int)M-1; j > -1 && rem > 0; j--)
             {
-                Map.Entry me = (Map.Entry)i.next();
-                long a = (long) me.getKey(), b = (long) me.getValue();
+                long a = (long) list.get(j).getX();
+                long b = (long) list.get(j).getY();;
                 //System.out.println(a + " "+b);
                 lcm = lcm* b / gcd(lcm,b);
                 ans = ans + (rem-N/lcm)*a;
@@ -67,4 +72,16 @@ class Codechef
             System.out.println(ans);
         }
 	}
+    static void sortList(ArrayList<Pair> list)
+    {
+        Collections.sort(list, new Comparator<Pair>()
+        {
+        @Override              
+        public int compare(final Pair p1, final Pair p2)
+        {
+            
+                return p1.x - p2.x;
+        }
+        });
+    }
 }
