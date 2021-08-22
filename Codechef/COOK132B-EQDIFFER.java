4bcdea2d555fe47109ef907c964e2cbc77ee9955
Aavesh K <Aavesh.bit@gmail.com>
2021-08-22 22:45:59 +0530

    Codechef Cookoff August

---
+++ Codechef/COOK132B-EQDIFFER.java
@@ -0,0 +1,89 @@
+/*
+
+Sample Input 1
+
+3
+2
+1 2
+5
+2 5 1 2 2
+4
+1 2 1 2
+
+Sample Output 1
+
+0
+2
+2
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class EQUDIFFER
+{
+    static int MAX(int x, int y){return ((x > y) ? x : y );}
+    static int min(int x, int y){return ((x < y) ? x : y );}
+    static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
+    static int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+	static boolean isPowerOfTwo(int n){if(n==0)return false;return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==(int)(Math.floor(((Math.log(n) / Math.log(2)))));}
+	static int countDigits(long l)
+	{
+			if (l >= 1000000000000000000L) return 19;
+			if (l >= 100000000000000000L) return 18;
+			if (l >= 10000000000000000L) return 17;
+			if (l >= 1000000000000000L) return 16;
+			if (l >= 100000000000000L) return 15;
+			if (l >= 10000000000000L) return 14;
+			if (l >= 1000000000000L) return 13;
+			if (l >= 100000000000L) return 12;
+			if (l >= 10000000000L) return 11;
+			if (l >= 1000000000L) return 10;
+			if (l >= 100000000L) return 9;
+			if (l >= 10000000L) return 8;
+			if (l >= 1000000L) return 7;
+			if (l >= 100000L) return 6;
+			if (l >= 10000L) return 5;
+			if (l >= 1000L) return 4;
+			if (l >= 100L) return 3;
+			if (l >= 10L) return 2;
+			return 1;
+	}
+	static void sort2D(int[][] Arr){Arrays.sort(Arr, (a, b) -> Double.compare(a[0], b[0]));}
+    public static void main (String[] args) throws IOException
+	{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(in.readLine());
+		while( T -- > 0)
+		{
+			int N = Integer.parseInt(in.readLine());
+			String lines = in.readLine();
+			String[] strs = lines.trim().split("\\s+");
+			HashMap<Integer, Integer> freq= new HashMap<Integer, Integer>();
+			for(int i = 0; i < N; i++)
+			{
+				int x = Integer.parseInt(strs[i]);
+				if(freq.containsKey(x))
+					freq.put(x,freq.get(x)+1);
+				else
+					freq.put(x,1);
+			}
+			int max = -1;
+			for (int entry : freq.keySet())
+			{
+				if(freq.get(entry)> max)
+					max = freq.get(entry);
+			}
+			int ans = 0;
+			if(N <= 2)
+				ans = 0;
+			else if(max == 1)
+			    ans = N - 2;
+			else
+			    ans = N- max;
+			System.out.println(ans);
+		}
+	}
+}
