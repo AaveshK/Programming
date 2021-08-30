60c86117980ce164efbc206b0498e89ce4d03b5c
Aavesh K <Aavesh.bit@gmail.com>
2021-08-30 18:54:57 +0530

    30th August

---
+++ Codeforces/318A-EvenOdds.java
@@ -0,0 +1,68 @@
+/*
+
+Sample Input 1
+
+10 3
+
+Sample Output 1
+
+5
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class EvenOdds
+{
+    static int max(int x, int y){return ((x > y) ? x : y );}
+    static int min(int x, int y){return ((x < y) ? x : y );}
+    static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
+    static int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+	static boolean isPowerOfTwo(int n){if(n==0)return false;return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==(int)(Math.floor(((Math.log(n) / Math.log(2)))));}
+	static int countDigits(long l)
+	{
+			if (l >= 1000000000000000000L) return 19;
+			if (l >= 100000000000000000L) return 18;
+			if (l >= 10000000000000000L) return 17;
+			if (l >= 1000000000000000L) return 16;
+			if (l >= 100000000000000L) return 15;
+			if (l >= 10000000000000L) return 14;
+			if (l >= 1000000000000L) return 13;
+			if (l >= 100000000000L) return 12;
+			if (l >= 10000000000L) return 11;
+			if (l >= 1000000000L) return 10;
+			if (l >= 100000000L) return 9;
+			if (l >= 10000000L) return 8;
+			if (l >= 1000000L) return 7;
+			if (l >= 100000L) return 6;
+			if (l >= 10000L) return 5;
+			if (l >= 1000L) return 4;
+			if (l >= 100L) return 3;
+			if (l >= 10L) return 2;
+			return 1;
+	}
+	static void sort2D(int[][] Arr){Arrays.sort(Arr, (a, b) -> Double.compare(a[0], b[0]));}
+    public static void main (String[] args) throws IOException
+	{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int T = 1;
+		while( T -- > 0)
+		{
+			String lines = in.readLine();
+			String[] strs = lines.trim().split("\\s+");
+			long N = Long.parseLong(strs[0]);
+			long K = Long.parseLong(strs[1]);
+			long temp =  (N+1)/2;
+			if(K <= temp)
+			{
+				System.out.println(K*2-1);
+			}
+			else
+			{
+				System.out.println((K-temp)*2);
+			}
+		}
+	}
+}
