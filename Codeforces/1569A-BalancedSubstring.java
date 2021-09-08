8e068e76dfc847f71c04efea43363680d9e01a1a
Aavesh K <Aavesh.bit@gmail.com>
2021-09-08 22:44:36 +0530

    Educational Codeforces Round 113 A

---
+++ Codeforces/1569A-BalancedSubstring.java
@@ -0,0 +1,88 @@
+/*
+
+Sample Input 1
+
+4
+1
+a
+6
+abbaba
+6
+abbaba
+9
+babbabbaa
+
+Sample Output 1
+
+-1 -1
+1 6
+3 6
+2 5
+
+*/
+import java.io.*;
+import java.util.*;
+  
+class R1569A
+{
+    static int MAX(int x, int y){return ((x > y) ? x : y );}
+    static int MIN(int x, int y){return ((x < y) ? x : y );}
+    static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
+    static int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+    static boolean isPowerOfTwo(int n){if(n==0)return false;return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==(int)(Math.floor(((Math.log(n) / Math.log(2)))));}
+    static int countDigits(long l)
+    {
+            if (l >= 1000000000000000000L) return 19;
+            if (l >= 100000000000000000L) return 18;
+            if (l >= 10000000000000000L) return 17;
+            if (l >= 1000000000000000L) return 16;
+            if (l >= 100000000000000L) return 15;
+            if (l >= 10000000000000L) return 14;
+            if (l >= 1000000000000L) return 13;
+            if (l >= 100000000000L) return 12;
+            if (l >= 10000000000L) return 11;
+            if (l >= 1000000000L) return 10;
+            if (l >= 100000000L) return 9;
+            if (l >= 10000000L) return 8;
+            if (l >= 1000000L) return 7;
+            if (l >= 100000L) return 6;
+            if (l >= 10000L) return 5;
+            if (l >= 1000L) return 4;
+            if (l >= 100L) return 3;
+            if (l >= 10L) return 2;
+            return 1;
+    }
+    static void sort2D(int[][] Arr){Arrays.sort(Arr, (a, b) -> Double.compare(a[0], b[0]));}
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        if (System.getProperty("ONLINE_JUDGE") == null)
+        {
+            try 
+            {
+                in = new BufferedReader(new FileReader("input.txt"));
+                System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+            }
+            catch (Exception e) {
+                //System.out.println("Exception");
+            }
+        }
+        int T = Integer.parseInt(in.readLine());
+        while( T -- > 0)
+        {
+            int N = Integer.parseInt(in.readLine());
+            String s = in.readLine();
+            char[] ch = s.toCharArray();
+            int flag = 0;
+            for(int i = 0; i < N - 1 && flag != 1; i++)
+            {
+                if(ch[i] != ch[i+1])
+                    {System.out.println((i+1)+" "+(i+2)); flag = 1;}
+            }
+            if(flag == 1)
+                continue;
+            else
+                System.out.println("-1 -1");
+        }
+    }
+}
