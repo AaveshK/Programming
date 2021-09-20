91a2cc9f6f4cebcc6f0fc8b3b52f306b8516fe30
Aavesh K <Aavesh.bit@gmail.com>
2021-09-20 23:11:12 +0530

    Codeforces Educational Round 114 B

---
+++ Codeforces/1574B-CombinatoricsHomework.java
@@ -0,0 +1,79 @@
+/*
+
+Sample Input 1
+
+
+
+Sample Output 1
+
+
+*/
+import java.io.*;
+import java.util.*;
+  
+public class R1574B
+{
+    static int MAX(int x, int y){return ((x > y) ? x : y );}
+    static int MIN(int x, int y){return ((x < y) ? x : y );}
+    static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
+    static int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+    static boolean isPowerOfTwo(int n){if(n==0)return false;return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==(int)(Math.floor(((Math.log(n) / Math.log(2)))));}
+    static int countDigits(long l)
+    {
+            if (l >= 1000000000000000000L) return 19;
+            if (l >= 100000000000000000L) return 18;
+            if (l >= 10000000000000000L) return 17;
+            if (l >= 1000000000000000L) return 16;
+            if (l >= 100000000000000L) return 15;
+            if (l >= 10000000000000L) return 14;
+            if (l >= 1000000000000L) return 13;
+            if (l >= 100000000000L) return 12;
+            if (l >= 10000000000L) return 11;
+            if (l >= 1000000000L) return 10;
+            if (l >= 100000000L) return 9;
+            if (l >= 10000000L) return 8;
+            if (l >= 1000000L) return 7;
+            if (l >= 100000L) return 6;
+            if (l >= 10000L) return 5;
+            if (l >= 1000L) return 4;
+            if (l >= 100L) return 3;
+            if (l >= 10L) return 2;
+            return 1;
+    }
+    static void sort2D(int[][] Arr){Arrays.sort(Arr, (a, b) -> Double.compare(a[0], b[0]));}
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        if (System.getProperty("ONLINE_JUDGE") == null)
+        {
+            try 
+            {
+                in = new BufferedReader(new FileReader("input.txt"));
+                System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+            }
+            catch (Exception e) {
+                //System.out.println("Exception");
+            }
+        }
+        int T = Integer.parseInt(in.readLine());
+        while( T -- > 0)
+        {
+            String lines = in.readLine();
+            String[] strs = lines.trim().split("\\s+");
+            int[] ar = new int[4];
+            for(int i = 0; i < 4; i++)
+            {
+                ar[i] = Integer.parseInt(strs[i]);
+            }
+            int min = MIN(MIN(ar[0],ar[1]),ar[2]);
+            int max = MAX(MAX(ar[0],ar[1]),ar[2]);
+            int pair = ar[0]+ar[1]+ar[2]-3;
+            if(ar[3] > pair)
+                System.out.println("NO");
+            else if(ar[3] >= (max - min - (pair+3-max-min) - 1))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
