e7053e51b8e8a5b05a8d2071b2659d22b34b0720
Aavesh K <Aavesh.bit@gmail.com>
2021-10-27 22:39:16 +0530

    October Lunchtime 2021 Division 2 Exactly N plus 1 Values code

---
+++ Codechef/LTIME101B_N1VALUES.java
@@ -0,0 +1,94 @@
+/*
+
+Sample Input 1
+
+2
+3
+4
+
+Sample Output 1
+
+1 2 2 3
+1 2 3 3 7
+
+*/
+import java.io.*;
+import java.util.*;
+  
+public class LTIME101BQ1
+{
+    static int MAX(int x, int y){return ((x > y) ? x : y );}
+    static int MIN(int x, int y){return ((x < y) ? x : y );}
+    static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
+    static int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+    static boolean isPowerOfTwo(int n){if(n==0)return false;return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==(int)(Math.floor(((Math.log(n) / Math.log(2)))));}
+    static int countDigits(long l)
+    {
+            if (l >= 1000000000000000000L) return 19;
+            if (l >= 100000000000000000L) return 18;
+            if (l >= 10000000000000000L) return 17;
+            if (l >= 1000000000000000L) return 16;
+            if (l >= 100000000000000L) return 15;
+            if (l >= 10000000000000L) return 14;
+            if (l >= 1000000000000L) return 13;
+            if (l >= 100000000000L) return 12;
+            if (l >= 10000000000L) return 11;
+            if (l >= 1000000000L) return 10;
+            if (l >= 100000000L) return 9;
+            if (l >= 10000000L) return 8;
+            if (l >= 1000000L) return 7;
+            if (l >= 100000L) return 6;
+            if (l >= 10000L) return 5;
+            if (l >= 1000L) return 4;
+            if (l >= 100L) return 3;
+            if (l >= 10L) return 2;
+            return 1;
+    }
+    static void sort2D(int[][] Arr){Arrays.sort(Arr, (a, b) -> Double.compare(a[0], b[0]));}
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        if (System.getProperty("ONLINE_JUDGE") == null)
+        {
+            try 
+            {
+                in = new BufferedReader(new FileReader("input.txt"));
+                System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+            }
+            catch (Exception e) {
+                //System.out.println("Exception");
+            }
+        }
+        int T = Integer.parseInt(in.readLine());
+        while( T -- > 0)
+        {
+            int N = Integer.parseInt(in.readLine());
+            solver(N);
+        }
+    }
+    static void solver(int N)
+    {
+        if(N == 1)
+        {
+            System.out.println("1 1");
+            return;
+        }
+        if(N == 2)
+        {
+            System.out.println("1 1 2");
+            return;
+        }
+        List<Long> list = new ArrayList<>();
+        list.add((long)1);
+        list.add((long)2);
+        list.add((long)2);
+        list.add((long)3);
+        for(int i = 3; i <= N; i++)
+        {
+            list.add((long)Math.pow(2,i));
+        }
+        for(int i = 0; i <= N; i++)
+            System.out.print(list.get(i)+" ");
+        System.out.println();
+    }
+}
