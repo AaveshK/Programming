e9bfa20d51d0387594e5b6086b0633c070d326c0
Aavesh K <Aavesh.bit@gmail.com>
2021-11-02 23:54:08 +0530

    Path Sum code using Recursion

---
+++ Leetcode/112. Path Sum/PSRecursion.java
@@ -0,0 +1,12 @@
+class Solution
+{
+    public boolean hasPathSum(TreeNode root, int targetSum)
+    {
+        if(root == null)
+            return false;
+        else if(root.left == root.right && root.left == null && targetSum - root.val == 0)
+            return true;
+        else
+            return hasPathSum(root.left,targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
