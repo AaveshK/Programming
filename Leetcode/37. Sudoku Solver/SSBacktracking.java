3c6e69899d9d20b36afbd65f1d0fe6d066c72661
Aavesh K <Aavesh.bit@gmail.com>
2021-11-04 22:31:33 +0530

    Sudoku Solver code using Backtracking

---
+++ Leetcode/37. Sudoku Solver/SSBacktracking.java
@@ -0,0 +1,45 @@
+class Solution
+{
+    public void solveSudoku(char[][] board)
+    {
+        solver(board);
+    }
+    static boolean solver(char[][] board)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    for(char c = '1'; c <= '9'; c++)
+                    {
+                        if(isValid(board,i,j,c))
+                        {
+                            board[i][j] = c;
+                            if(solver(board) == true)
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    static boolean isValid(char[][] board, int row, int col, char c)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            if(board[i][col] == c)
+                return false;
+            if(board[row][i] == c)
+                return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c)
+                return false;
+        }
+        return true;
+    }
+}
