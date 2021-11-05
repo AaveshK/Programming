de67b295c0bc583bad4bfbdd7526b54a03584d7c
Aavesh K <Aavesh.bit@gmail.com>
2021-11-05 22:55:35 +0530

    Codechef November challenge Equal Coins Java

---
+++ Codechef/NOV21B_EQUALCOIN.java
@@ -0,0 +1,85 @@
+/*
+
+Sample Input 1
+
+4
+2 2
+1 3
+4 0
+1 10
+
+Sample Output 1
+
+YES
+NO
+YES
+NO
+
+*/
+import java.io.*;
+import java.util.*;
+  
+public class NOV21B_EQUALCOIN
+{
+    static long MAX(long x, long y){return ((x > y) ? x : y );}
+    static long MIN(long x, long y){return ((x < y) ? x : y );}
+    static int sum(int[] A){int sum = 0;for(int i=0; i<A.length; i++)sum+=A[i];return (sum);}
+    static int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);}
+    static boolean isPowerOfTwo(int n){if(n==0)return false;return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==(int)(Math.floor(((Math.log(n) / Math.log(2)))));}
+    static int countDigits(long l)
+    {
+            if (l >= 1000000000000000000L) return 19;
+            if (l >= 100000000000000000L) return 18;
+            if (l >= 10000000000000000L) return 17;
+            if (l >= 1000000000000000L) return 16;
+            if (l >= 100000000000000L) return 15;
+            if (l >= 10000000000000L) return 14;
+            if (l >= 1000000000000L) return 13;
+            if (l >= 100000000000L) return 12;
+            if (l >= 10000000000L) return 11;
+            if (l >= 1000000000L) return 10;
+            if (l >= 100000000L) return 9;
+            if (l >= 10000000L) return 8;
+            if (l >= 1000000L) return 7;
+            if (l >= 100000L) return 6;
+            if (l >= 10000L) return 5;
+            if (l >= 1000L) return 4;
+            if (l >= 100L) return 3;
+            if (l >= 10L) return 2;
+            return 1;
+    }
+    static void sort2D(int[][] Arr){Arrays.sort(Arr, (a, b) -> Double.compare(a[0], b[0]));}
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        if (System.getProperty("ONLINE_JUDGE") == null)
+        {
+            try 
+            {
+                in = new BufferedReader(new FileReader("input.txt"));
+                System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+            }
+            catch (Exception e) {
+                //System.out.println("Exception");
+            }
+        }
+        int T = Integer.parseInt(in.readLine());
+        while( T -- > 0)
+        {
+            String[] strs = in.readLine().trim().split("\\s+");
+            System.out.println(solver(Integer.parseInt(strs[0]),Integer.parseInt(strs[1])));
+        }
+    }
+    static String solver(int X, int Y)
+    {
+        long total = X + 2* Y;
+        if((X & 1) != 0)
+            return "NO";
+        if((Y&1) != 0)
+        {
+            if(X < 2)
+                return "NO";
+        }
+        return "YES";
+    }
+}
